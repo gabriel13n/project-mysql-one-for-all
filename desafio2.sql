8edc8643594b437d554f9f1fde7ee30bf7e6640e
Gabriel Cantuária <gabriel13n@gmail.com>
2022-11-10 22:03:25 -0300

    requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+SELECT 
+    COUNT(DISTINCT so.id) AS 'cancoes',
+    COUNT(DISTINCT ar.id) AS 'artistas',
+    COUNT(DISTINCT al.id) AS 'albuns'
+FROM
+    SpotifyClone.songs AS so,
+    SpotifyClone.artists AS ar,
+    SpotifyClone.albums AS al;
